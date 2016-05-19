10895e0ee07647ae8000270e9b5762b4c053604a
Cremate91 <andreas.burger91@gmail.com>
2016-05-19 23:50:05 +0200

    Add a Layout and start do creat logic

--- app/src/main/java/de/cremate_gamings/tictactoe/MainActivity.java
+++ app/src/main/java/de/cremate_gamings/tictactoe/MainActivity.java
@@ -2,12 +2,39 @@ package de.cremate_gamings.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+/**
+ * Tic Tac Toe Game
+ * ---
+ *
+ * Code by cremate-gamings
+ * First android-app
+ *
+ * @author Andreas Burger
+ */
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private int playCount = 0;
+    private float progress = 0;
+    private int MAXPLAYCOUNT = 9;
+
+
+    private Button button1;
+
+    private Button[] buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+
 }
