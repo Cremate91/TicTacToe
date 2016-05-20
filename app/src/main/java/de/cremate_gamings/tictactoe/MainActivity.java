541986fd40c50383829d62f377b8199355240912
Cremate91 <andreas.burger91@gmail.com>
2016-05-21 01:13:25 +0200

    Add some changes

--- app/src/main/java/de/cremate_gamings/tictactoe/MainActivity.java
+++ app/src/main/java/de/cremate_gamings/tictactoe/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Tic Tac Toe Game
@@ -23,6 +27,12 @@ public class MainActivity extends AppCompatActivity {
     private float progress = 0;
     private int MAXPLAYCOUNT = 9;
     private boolean played = false;
+    TextView pfeil;
+    TextView winner;
+    TextView progressTV;
+    ProgressBar progressBar;
+
+    Button reset;
     Button[] buttons;
 
     @Override
@@ -38,6 +48,13 @@ public class MainActivity extends AppCompatActivity {
      * Init some Buttons and add an EventListener
      */
     private void init() {
+        pfeil = (TextView) findViewById(R.id.textView);
+        winner = (TextView) findViewById(R.id.textView4);
+        progressTV = (TextView) findViewById(R.id.textView6);
+
+        reset = (Button) findViewById(R.id.button10);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
         Button button1 = (Button) findViewById(R.id.button);
         Button button2 = (Button) findViewById(R.id.button2);
         Button button3 = (Button) findViewById(R.id.button3);
@@ -53,12 +70,10 @@ public class MainActivity extends AppCompatActivity {
         for (final Button button : buttons) {
             button.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
+
                     if (playCount <= 8) {
                         if (button.getText() == "") {
                             button.setText(whichPlayer());
-
-                        } else {
-
                         }
                     }else{
                         gameOver();
@@ -73,22 +88,56 @@ public class MainActivity extends AppCompatActivity {
      * Set the buttons Invisible and show the winner and restart the game
      */
     private void gameOver() {
+
         for(Button button : buttons){
 
             button.setVisibility(View.INVISIBLE);
+        }
+        if(playCount > 8){
+            Log.d("hi", "test");
+            winner.setText(R.string.no_winner);
+        }else{
+            //ToDo Brainlogic
+            //Log.d("hi", "ttttt");
+
+        }
+        winner.setVisibility(View.VISIBLE);
+        reset.setVisibility(View.VISIBLE);
+        reset.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                reset();
+            }
+        });
+    }
+
+
+    /**
+     * Reset the Game and UI
+     */
+    private void reset() {
+        playCount = 0;
+        pfeil.setText(R.string.pfeil_left);
+        winner.setVisibility(View.INVISIBLE);
+        reset.setVisibility(View.INVISIBLE);
+
+        for(Button button : buttons){
+            button.setText("");
+            button.setVisibility(View.VISIBLE);
         }
     }
 
 
     /**
      * Give the Player (X or Y) back which is set to the button
-     * @return
+     * @return String with X or Y
      */
     public String whichPlayer() {
         playCount++;
-        if (playCount % 2 == 0) {
+        if (playCount % 2 == 1) {
+            pfeil.setText(R.string.pfeil_right);
             return "x";
         } else {
+            pfeil.setText(R.string.pfeil_left);
             return "y";
         }
     }
